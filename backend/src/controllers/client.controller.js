2770f2e0f6d3d263a4a0292235240ddad462cd03
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2024-03-07 19:19:37 -0300

    Refactor  : controlador de clientes y consultas de clientes .

--- backend/src/controllers/client.controller.js
+++ backend/src/controllers/client.controller.js
@@ -8,22 +8,22 @@ const {
 const db = require("../database/db.conection");
 module.exports = {
   async getAll(req, res) {
-    const users = await db.query(GET_ALL_CLIENTS).then((res) => {
+    const client = await db.query(GET_ALL_CLIENTS).then((res) => {
       if (Object.keys(res).length === 0) {
         return null;
       } else {
         return res;
       }
     });
-    if (users) return res.status(STATUS_CODES.OK).json(users);
+    if (client) return res.status(STATUS_CODES.OK).json(client);
     else
       return res
         .status(STATUS_CODES.INTERNAL_SERVER_ERROR)
-        .json({ message: "An error occurred while trying to get the users" });
+        .json({ message: "An error occurred while trying to get the clients" });
   },
   async create(req, res) {
     const { name, email, phone } = req.body;
-    const user = await db
+    const client = await db
       .query(CREATE_CLIENT, [name, email, phone])
       .then((res) => {
         if (Object.keys(res).length === 0) {
@@ -32,19 +32,19 @@ module.exports = {
           return res;
         }
       });
-    if (user)
+    if (client)
       return res
         .status(STATUS_CODES.CREATED)
-        .json({ message: "A new user has by created", user });
+        .json({ message: "A new client has by created",  client });
     else
       return res
         .status(STATUS_CODES.INTERNAL_SERVER_ERROR)
-        .json({ message: "An error occurred while trying to insert the user" });
+        .json({ message: "An error occurred while trying to insert the client" });
   },
   async update(req, res) {
     const { id } = req.params;
     const { name, email, phone } = req.body;
-    const user = await db
+    const client = await db
       .query(UPDATE_CLIENT, [name, email, phone, id])
       .then((res) => {
         if (Object.keys(res).length === 0) {
@@ -54,34 +54,34 @@ module.exports = {
         }
       });
 
-    if (user) {
+    if (client) {
       return res
         .status(STATUS_CODES.OK)
-        .json({ message: "The user has been updated", user });
+        .json({ message: "The client has been updated",  client });
     } else {
       return res
         .status(STATUS_CODES.INTERNAL_SERVER_ERROR)
-        .json({ message: "An error occurred while trying to update the user" });
+        .json({ message: "An error occurred while trying to update the client" });
     }
   },
   async deleteOne(req, res) {
     const { id } = req.params;
-    const userDeleted = await db.query(DELETE_CLIENT, [id]).then((res) => {
+    const clientDeleted = await db.query(DELETE_CLIENT, [id]).then((res) => {
       if (Object.keys(res).length === 0) {
         return null;
       } else {
         return res;
       }
     });
-    if (userDeleted) {
+    if (clientDeleted) {
       return res
         .status(STATUS_CODES.OK)
-        .json({ message: "The next user has been deleted", user: userDeleted });
+        .json({ message: "The next client has been deleted",  clientDeleted });
     } else {
       return res
         .status(STATUS_CODES.NOT_FOUND)
         .json({
-          message: `The user with id : ${id} does not exist in the database`,
+          message: `The client with id : ${id} does not exist in the database`,
         });
     }
   },
--- backend/src/database/querys/client.querys.js
+++ backend/src/database/querys/client.querys.js
@@ -1,12 +1,12 @@
 module.exports = {
   CREATE_CLIENT:
-    "INSERT INTO users (name, email, phone) VALUES ($1, $2, $3) RETURNING *",
-  DELETE_CLIENT: "DELETE FROM users WHERE id = $1 RETURNING *",
-  GET_ALL_CLIENTS: "SELECT * FROM users",
+    "INSERT INTO clients (name, email, phone) VALUES ($1, $2, $3) RETURNING *",
+  DELETE_CLIENT: "DELETE FROM clients WHERE id = $1 RETURNING *",
+  GET_ALL_CLIENTS: "SELECT * FROM clients",
   UPDATE_CLIENT:
-    "UPDATE users SET name = $1, email = $2, phone = $3 WHERE id = $4 RETURNING *",
-  SELECT_BY_PHONE : "SELECT * FROM users WHERE phone = $1",
-  SELECT_BY_EMAIL : "SELECT * FROM users WHERE email = $1",
-  SELECT_BY_ID : "SELECT * FROM users WHERE id = $1",
-  SELECT_BY_NAME : "SELECT * FROM users WHERE name = $1",
+    "UPDATE clients SET name = $1, email = $2, phone = $3 WHERE id = $4 RETURNING *",
+  SELECT_BY_PHONE : "SELECT * FROM clients WHERE phone = $1",
+  SELECT_BY_EMAIL : "SELECT * FROM clients WHERE email = $1",
+  SELECT_BY_ID : "SELECT * FROM clients WHERE id = $1",
+  SELECT_BY_NAME : "SELECT * FROM clients WHERE name = $1",
 };
