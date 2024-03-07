f2ddc13b203057aca84d131237394742a1cd147e
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2024-03-07 18:23:28 -0300

    Agregadas consultas SELECT para buscar usuarios por teléfono, correo electrónico, ID y nombre

--- backend/src/database/querys/user.querys.js
+++ backend/src/database/querys/user.querys.js
@@ -5,4 +5,8 @@ module.exports = {
   GET_ALL_USERS: "SELECT * FROM users",
   UPDATE_USER:
     "UPDATE users SET name = $1, email = $2, phone = $3 WHERE id = $4 RETURNING *",
+  SELECT_BY_PHONE : "SELECT * FROM users WHERE phone = $1",
+  SELECT_BY_EMAIL : "SELECT * FROM users WHERE email = $1",
+  SELECT_BY_ID : "SELECT * FROM users WHERE id = $1",
+  SELECT_BY_NAME : "SELECT * FROM users WHERE name = $1",
 };
