8d687e38e04cae0c7da407a6a2a88a375e0f5fa8
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2024-03-07 18:49:28 -0300

    Añadir actualización de usuarios en controlador de usuarios y correccion de verificacion si la consulta fue bien susedida en la base de datos

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,9 +1,19 @@
 const { STATUS_CODES } = require("../utils/app.utils");
-const { CREATE_USER,GET_ALL_USERS  } = require("../database/querys/user.querys");
+const {
+  CREATE_USER,
+  GET_ALL_USERS,
+  UPDATE_USER,
+} = require("../database/querys/user.querys");
 const db = require("../database/db.conection");
 module.exports = {
   async getAll(req, res) {
-    const users = await db.query(GET_ALL_USERS);
+    const users = await db.query(GET_ALL_USERS).then((res) => {
+      if (Object.keys(res).length === 0) {
+        return null;
+      } else {
+        return res;
+      }
+    });
     if (users) return res.status(STATUS_CODES.OK).json(users);
     else
       return res
@@ -12,7 +22,15 @@ module.exports = {
   },
   async create(req, res) {
     const { name, email, phone } = req.body;
-    const user = await db.query(CREATE_USER, [name, email, phone]);
+    const user = await db
+      .query(CREATE_USER, [name, email, phone])
+      .then((res) => {
+        if (Object.keys(res).length === 0) {
+          return null;
+        } else {
+          return res;
+        }
+      });
     if (user)
       return res
         .status(STATUS_CODES.CREATED)
@@ -23,7 +41,27 @@ module.exports = {
         .json({ message: "An error occurred while trying to insert the user" });
   },
   async update(req, res) {
-    res.status(STATUS_CODES.OK).send("PUT from user routes");
+    const { id } = req.params;
+    const { name, email, phone } = req.body;
+    const user = await db
+      .query(UPDATE_USER, [name, email, phone, id])
+      .then((res) => {
+        if (Object.keys(res).length === 0) {
+          return null;
+        } else {
+          return res;
+        }
+      });
+
+    if (user) {
+      return res
+        .status(STATUS_CODES.OK)
+        .json({ message: "The user has been updated", user });
+    } else {
+      return res
+        .status(STATUS_CODES.INTERNAL_SERVER_ERROR)
+        .json({ message: "An error occurred while trying to update the user" });
+    }
   },
   async deleteOne(req, res) {
     res.status(STATUS_CODES.OK).send("DELETE from user routes");
