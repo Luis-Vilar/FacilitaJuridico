bbf62e678f9aed5496cc90859134a2da5ec819d0
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2024-03-07 18:20:46 -0300

    Añadir consultas para eliminar, obtener todos los usuarios y actualizar un usuario en user.querys.js

--- backend/src/database/querys/user.querys.js
+++ backend/src/database/querys/user.querys.js
@@ -1,4 +1,8 @@
 module.exports = {
   CREATE_USER:
     "INSERT INTO users (name, email, phone) VALUES ($1, $2, $3) RETURNING *",
+  DELETE_USER: "DELETE FROM users WHERE id = $1",
+  GET_ALL_USERS: "SELECT * FROM users",
+  UPDATE_USER:
+    "UPDATE users SET name = $1, email = $2, phone = $3 WHERE id = $4 RETURNING *",
 };
