2b035583f87f8e568ee1ad98b369e0340f3d5ace
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2024-03-07 16:51:24 -0300

    Agregada configuración de base de datos y prueba de conexión a la clase Server

--- backend/src/server/index.js
+++ backend/src/server/index.js
@@ -1,17 +1,42 @@
 const express = require("express");
 const app = express();
 const routes = require("../routes/index");
+const dotenv = require("dotenv");
+const pgp = require("pg-promise")(/* options */);
 
 class Server {
   app;
+  db;
   constructor() {
+    dotenv.config();
+    this.db = pgp(process.env.DATABASE_URL);
     this.app = app;
     this.app.use(express.json());
     this.route(this.app);
+    this.dbTestConection();
+
     this.port = process.env.PORT || 3000;
   }
   route(app) {
     app.use(routes);
+  }
+  dbTestConection() {
+    return this.db
+      .one("SELECT $1 AS value", 123)
+      .then((data) => {
+        if (data) {
+          console.log("Everything is fine, the database is online");
+        }
+      })
+      .catch((error) => {
+        console.log(
+          "An error occurred while trying to connect to the database:",
+          error
+        );
+      });
+  }
+  getDb() {
+    return this.db;
   }
 
   start() {
