45e0d8110da1d7c0b775f1a20e282d8fbff5603d
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2024-03-07 17:39:32 -0300

    Añadir configuración de base de datos y conexión

--- backend/src/server/index.js
+++ backend/src/server/index.js
@@ -1,27 +1,24 @@
+require("dotenv").config()
 const express = require("express");
 const app = express();
 const routes = require("../routes/index");
-const dotenv = require("dotenv");
-const pgp = require("pg-promise")(/* options */);
+
+const db = require("../database/db.conection");
 
 class Server {
   app;
-  db;
   constructor() {
-    dotenv.config();
-    this.db = pgp(process.env.DATABASE_URL);
     this.app = app;
     this.app.use(express.json());
     this.route(this.app);
     this.dbTestConection();
-
     this.port = process.env.PORT || 3000;
   }
   route(app) {
     app.use(routes);
   }
   dbTestConection() {
-    return this.db
+     db
       .one("SELECT $1 AS value", 123)
       .then((data) => {
         if (data) {
@@ -30,15 +27,11 @@ class Server {
       })
       .catch((error) => {
         console.log(
-          "An error occurred while trying to connect to the database:",
+          "An error occurred while trying to connect to the database: ",
           error
         );
       });
   }
-  getDb() {
-    return this.db;
-  }
-
   start() {
     this.app.listen(this.port, () => {
       console.log(`Server running on port ${this.port}`);
