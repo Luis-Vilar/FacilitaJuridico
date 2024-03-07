55e2b1308e72e1de77cb0880faa671519a1bfc4d
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2024-03-07 20:47:04 -0300

    Agregada ruta de direcciones a las rutas existentes

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,7 +1,8 @@
 const { Router } = require('express')
 const router = new Router()
 const clientsRoutes = require('./clients.routes')
+const addresRoutes = require('./addres.routes')
 
-router.use([clientsRoutes])
+router.use([clientsRoutes, addresRoutes])
 
 module.exports = router
