80896184007a84fbb70d144f95895571368d9740
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2024-03-07 18:16:29 -0300

    Agregada funcionalidad de creación de usuarios

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,10 +1,21 @@
 const { STATUS_CODES } = require("../utils/app.utils");
+const { CREATE_USER } = require("../database/querys/user.querys");
+const db = require("../database/db.conection");
 module.exports = {
   async getAll(req, res) {
     res.status(STATUS_CODES.OK).send("GET from user routes");
   },
   async create(req, res) {
-    res.status(STATUS_CODES.CREATED).send("POST from user routes");
+    const { name, email, phone } = req.body;
+    const user = await db.query(CREATE_USER, [name, email, phone]);
+    if (user)
+      return res
+        .status(STATUS_CODES.CREATED)
+        .json({ message: "A new user has by created", user });
+    else
+      return res
+        .status(STATUS_CODES.INTERNAL_SERVER_ERROR)
+        .json({ message: "An error occurred while trying to insert the user" });
   },
   async update(req, res) {
     res.status(STATUS_CODES.OK).send("PUT from user routes");
