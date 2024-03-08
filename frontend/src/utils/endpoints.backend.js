31c36b96a39eedd46015d5fdfd6db81efb453769
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2024-03-08 01:15:26 -0300

    Agrega endpoints para consumir la API

---
+++ frontend/src/utils/endpoints.backend.js
@@ -0,0 +1,8 @@
+/*Apenas este endereço deve ser alterado para 
+o URL do backend quando for para produçao*/
+const BASE_URL = "http://localhost:3000/";
+
+// Endpoints para consumo da API
+export const endpoints = {
+  GET_BEST_ROUTE: `${BASE_URL}/addres`,
+};
