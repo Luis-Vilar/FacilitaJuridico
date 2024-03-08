65d7fb0f5bd775fe525deb36f92796c0b50952e6
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2024-03-08 00:48:14 -0300

    Actualizar encabezado de la barra de navegación

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,11 @@
 import Container from "react-bootstrap/Container";
-import {Navbar, Nav} from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
 
 function Header() {
   return (
-    <Navbar bg="dark" variant="dark">
-      <Container>
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container fluid>
         <Navbar.Brand href="/">
           <img
             alt="Logo Fake da Facilita Jurídico"
@@ -16,11 +16,14 @@ function Header() {
           />
           Facilita Jurídico
         </Navbar.Brand>
-        <Nav className="me-auto">
+        <Navbar.Toggle className="justify-content-end" />
+        <Navbar.Collapse className="justify-content-end">
+          <Nav >
             <Nav.Link href="/cadastro">Cadastrar CLientes</Nav.Link>
             <Nav.Link href="/buscar">Buscar Clientes</Nav.Link>
             <Nav.Link href="/generador/melhorRota">Generar Ruta</Nav.Link>
           </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   );
