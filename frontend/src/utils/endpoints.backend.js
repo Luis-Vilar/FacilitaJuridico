a807c2c8d6837185b0d2228d72e8c9be29d397ab
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2024-03-08 01:45:48 -0300

    Actualizar URL del backend para producción

--- frontend/src/utils/endpoints.backend.js
+++ frontend/src/utils/endpoints.backend.js
@@ -1,6 +1,6 @@
 /*Apenas este endereço deve ser alterado para 
 o URL do backend quando for para produçao*/
-const BASE_URL = "http://localhost:3000/";
+const BASE_URL = "http://localhost:3000";
 
 // Endpoints para consumo da API
 export const endpoints = {
