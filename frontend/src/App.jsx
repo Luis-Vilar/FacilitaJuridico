828d9875d41e3579d357cc2fbb5dede5581cfb59
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2024-03-08 00:48:37 -0300

    Agregadas rutas de búsqueda, registro y generación de rutas en App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
 import Table from "./components/TableComponent";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Busca from "./pages/Busca";
+import Cadastro from "./pages/Cadastro";
+import GeneradorDeRotas from "./pages/GeneradorDeRotas";
 
 import { useState } from "react";
 import { mockedData } from "./mocks/clientsData";
-
 
 function App() {
   const [clients, setClients] = useState(mockedData);
@@ -13,10 +15,10 @@ function App() {
       <Header />
       <BrowserRouter>
         <Routes>
-          <Route
-            path="/"
-            element={<Table data={clients}  />}
-          />
+          <Route path="/" element={<Table data={clients} />} />
+          <Route path="/cadastro" element={<Cadastro />} />
+          <Route path="/buscar" element={<Busca />} />
+          <Route path="/generador/melhorRota" element={<GeneradorDeRotas />} />
         </Routes>
       </BrowserRouter>
     </>
