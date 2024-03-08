1642ff890c9ca08560af99d87685c52d3fbd0da0
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2024-03-08 02:54:14 -0300

    Agregado contenedor y título a GeneradorDeRotas.jsx

--- frontend/src/pages/GeneradorDeRotas.jsx
+++ frontend/src/pages/GeneradorDeRotas.jsx
@@ -1,19 +1,25 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { Container } from "react-bootstrap";
 
 import TableComponent from "../components/TableComponent";
 import { endpoints } from "../utils/endpoints.backend";
 const { GET_BEST_ROUTE } = endpoints;
 
 function GeneradorDeRotas() {
-  const [routes, setRoutes] = useState([{await : 'loading...'}]);
+  const [routes, setRoutes] = useState([{ await: "loading..." }]);
   useEffect(() => {
     axios.get(GET_BEST_ROUTE).then((response) => {
       setRoutes(response.data);
     });
   }, []);
-  
-  return <TableComponent data={routes} />;
+
+  return (
+    <Container  className="p-4">
+      <h1>Gerador de Rotas</h1>
+      <TableComponent data={routes} />
+    </Container>
+  );
 }
 
 export default GeneradorDeRotas;
