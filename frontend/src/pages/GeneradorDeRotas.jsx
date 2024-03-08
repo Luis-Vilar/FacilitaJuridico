c4f4af0eca2aaaa741d2177c27b42d5d8adb09c1
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2024-03-08 01:46:00 -0300

    Agregada lógica para obtener la mejor ruta y mostrarla en una tabla

--- frontend/src/pages/GeneradorDeRotas.jsx
+++ frontend/src/pages/GeneradorDeRotas.jsx
@@ -1,5 +1,19 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+import TableComponent from "../components/TableComponent";
+import { endpoints } from "../utils/endpoints.backend";
+const { GET_BEST_ROUTE } = endpoints;
+
 function GeneradorDeRotas() {
-  return <div>GeneradorDeRotas</div>;
+  const [routes, setRoutes] = useState([{await : 'loading...'}]);
+  useEffect(() => {
+    axios.get(GET_BEST_ROUTE).then((response) => {
+      setRoutes(response.data);
+    });
+  }, []);
+  
+  return <TableComponent data={routes} />;
 }
 
 export default GeneradorDeRotas;
