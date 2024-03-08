3a2058239c25ac8c53ada7d6f0145ff380fff256
Luis Vilar <luis_velazquez@estudante.sesisenai.org.br>
2024-03-08 00:07:39 -0300

    Agrega componente de tabla y datos simulados a la aplicación

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,15 @@
 import Header from "./components/Header";
+import Table from "./components/TableComponent";
+import { useState } from "react";
+import { mockedData } from "./mocks/clientsData";
+
+
 function App() {
+  const [clients, setClients] = useState(mockedData);
   return (
     <>
       <Header />
+      <Table data={clients} />
     </>
   );
 }
